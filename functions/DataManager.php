5c2a5605b14fcb8e706528348b4464b908d79d6d
Ksieciu <antelius14@gmail.com>
2020-06-04 14:55:13 +0200

    Added DataManager class with function showing champions icons

---
+++ functions/DataManager.php
@@ -0,0 +1,48 @@
+<?php
+
+
+class DataManager{
+
+    private $api_url; // = 'http://localhost/webservices/projectLoL/php-champions-rest-api/api/champion/';
+    // public $champion_data;
+
+    public function __construct($url){
+        $this->api_url = $url;
+    }
+
+
+    public function show_champs_icons($api_func){
+        $url = $this->api_url . $api_func;
+        $json_data = file_get_contents($url);
+        $champion_data = json_decode($json_data);
+        $cond = TRUE;
+        $counter = 0;
+        $records_num = count($champion_data);
+
+        // print data for debuggin purpose
+        // print_r($champion_data[0]->icon);
+        echo '<div class="container">';
+
+        // while for creating rows
+        while($cond){
+            echo '<div class="row">';
+
+            // loop for making number of columns, if no more champs, then break for and while loops.
+            for($i = 0; $i < 8; $i++){
+                if($counter >= $records_num){
+                    $cond = FALSE;
+                    break;
+                }
+                // making divs for columns, printing champions icons
+                echo '<div class-"col-md-4">';
+                echo '<img src="' . $champion_data[$counter]->icon . '">';
+                echo '</div>';
+                $counter++;
+            }
+        }
+    }
+
+    
+
+
+}
