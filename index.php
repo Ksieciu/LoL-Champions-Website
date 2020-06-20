0cc987199fb06fef33ae128d44b472d177d8758b
Ksieciu <antelius14@gmail.com>
2020-06-20 12:43:25 +0200

    hotfix

--- index.php
+++ index.php
@@ -5,6 +5,11 @@
     $monster_data = new MonsterManager('http://localhost/Lol-Heroes/LoL-Champions-Website/php-champions-rest-api/api/champion/');
    // $data = new DataManager('http://localhost/Lol-Heroes/LoL-Champions-Website/php-champions-rest-api/api/champion/');
 
+   session_start();
+   if(!$_SESSION['loggedin']){
+       $_SESSION['loggedin'] = false;
+   }
+
    if(isset($_POST['sbutton'])){
         $header_url = 'location: ' . 'details.php?name=' . $_POST['ch_search'];
         header($header_url);
@@ -62,7 +67,7 @@
         <path fill-rule="evenodd" d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
         <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/></a>
         </svg></li>
-        <li><?php session_start(); if($_SESSION["loggedin"] === true){ $_SESSION["email"];} ?> </li>
+        <li><?php  if($_SESSION["loggedin"] == true){ $_SESSION["email"];} ?> </li>
    
 </ul><hr>
 
